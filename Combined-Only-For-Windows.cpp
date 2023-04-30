43ec5cfc84d19a639d02c88f2c3885f5e6b3245e
Osama Mirza <99750385+0sama-Mirza@users.noreply.github.com>
2023-04-30 17:23:32 +0500

    Update Combined-Only-For-Windows.cpp

--- Combined-Only-For-Windows.cpp
+++ Combined-Only-For-Windows.cpp
@@ -490,29 +490,29 @@ class ttt:public game{
     	int gameover;
     public:
     	void reset(){	
-			matrix[0][0]={'1'};
-    		matrix[0][1]={'2'};
-			matrix[0][2]={'3'};
-			matrix[1][0]={'4'};
-    		matrix[1][1]={'5'};
-    		matrix[1][2]={'6'};
-    		matrix[2][0]={'7'};
-			matrix[2][1]={'8'};
-    		matrix[2][2]={'9'};
+			matrix[0][0]='1';
+    		matrix[0][1]='2';
+			matrix[0][2]='3';
+			matrix[1][0]='4';
+    		matrix[1][1]='5';
+    		matrix[1][2]='6';
+    		matrix[2][0]='7';
+			matrix[2][1]='8';
+    		matrix[2][2]='9';
     		player='X';
     		point1=0;
     		point2=0;
 		}
     	ttt(){
-    		matrix[0][0]={'1'};
-    		matrix[0][1]={'2'};
-			matrix[0][2]={'3'};
-			matrix[1][0]={'4'};
-    		matrix[1][1]={'5'};
-    		matrix[1][2]={'6'};
-    		matrix[2][0]={'7'};
-			matrix[2][1]={'8'};
-    		matrix[2][2]={'9'};
+    		matrix[0][0]='1';
+    		matrix[0][1]='2';
+			matrix[0][2]='3';
+			matrix[1][0]='4';
+    		matrix[1][1]='5';
+    		matrix[1][2]='6';
+    		matrix[2][0]='7';
+			matrix[2][1]='8';
+    		matrix[2][2]='9';
     		player='X';
     		point1=0;
     		point2=0;
